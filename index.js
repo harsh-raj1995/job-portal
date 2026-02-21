fd34713dc9d1f1a6bd57ce8250fbe82ec7be9c26
Harsh Raj <rajharsha806@gmail.com>
2026-02-21 12:57:25 +0530

    adding search which does not work

---
+++ index.js
@@ -0,0 +1,13 @@
+let s=document.querySelector(".src");
+let btn=document.querySelector(".search-btn");
+let input=document.querySelector(".search-input");
+btn.addEventListener("click",(event)=>{
+  event.preventDefault();
+  if(input.value==""){
+    alert("TYPE SOMETHING IN THE SEARCH BOX");
+    return;
+  }
+  let i=input.value.toLowerCase();
+    s.innerHTML='<p class="src">You searched for: '+i+'</p><p class="src">Sorry, no results found for "'+i+'". </p><p class="src">Please try again with different keywords.</p><br><br>';
+    input.value="";
+})
